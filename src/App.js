ab89e911d6d0d8bd009f2e4baf7230941ae9eebf
chamindujs <chamindujs@gmail.com>
2020-10-15 04:27:15 +0530

    Add active tab feature

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Dashboard from './components/layout/Dashboard'
